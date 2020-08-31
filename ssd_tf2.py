94732b8c270de4ce3c73dcb563b1b1e4152f2638
Milad.Niazy <milad.niazy@partsoftware.com>
2020-08-31 18:52:39 +0430

    limmit gpu memory growth

--- ssd_tf2.py
+++ ssd_tf2.py
@@ -27,6 +27,18 @@ if device_name != '/device:GPU:0':
   raise SystemError('GPU device not found')
 print('Found GPU at: {}'.format(device_name))
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+
 def load_image_into_numpy_array(path):
   """Load an image from file into a numpy array.
 
