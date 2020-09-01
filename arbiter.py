47ab40225a67e2e43a542d189b407513d85f594e
Milad.Niazy <milad.niazy@partsoftware.com>
2020-09-01 23:52:34 +0430

    working on queues

--- arbiter.py
+++ arbiter.py
@@ -13,14 +13,15 @@ class Arbiter:
         self.detector = MNR_Net.Detector(prototxt, model)
         self.detector.setRunMode(False)
         self.detector.initNet()
-        self.state=0
         self.processingCNN = Value('b', False)
+        self.firstDetect = Value('b', False)
         self.detectorP = Process(name='dnn', target=self.detectorThread)  # , args=[detector.runThread])
         self.detectorP.daemon = True  # background run
         self.trackerP = Process(name='track', target=self.trackerThread)  # , args=[detector.runThread])
         self.trackerP.daemon = True  # background run
         self.detectorP.start()
         self.trackerP.start()
+        self.counter=0
 
     def stop(self):
         self.runThread.value=False
@@ -28,40 +29,36 @@ class Arbiter:
         self.trackerP.join()
 
     def newImage(self, frame):
-        print ("new frame")
-        if(not self.processingCNN.value):
+        self.counter=self.counter+1
+        if(not self.processingCNN.value): # and self.counter%60==0):
+            print("detect", self.trackerQ.qsize())
             self.detectorInQ.put(frame)
+        if(self.trackerQ.qsize()>5):
+            self.trackerQ.get() # destroy frame
         self.trackerQ.put(frame)
 
     def detectorThread(self):
         while (self.runThread.value):
             if (not self.detectorInQ.empty()):
-                print("detect")
                 self.processingCNN.value=True
                 frame = self.detectorInQ.get()
                 detections = self.detector.serialDetector(frame)
                 self.detectorOutQ.put(detections['detection_out'])
                 self.processingCNN.value = False
+                self.firstDetect.value = True
 
     def trackerThread(self):
         detection=[0, 0, 0, 0]
         while (self.runThread.value):
-            if(not self.processingCNN.value and not self.detectorOutQ.empty()):
+            if(not self.detectorOutQ.empty()):
                 detection = self.detectorOutQ.get()
                 frame = self.trackerQ.get()
                 self.cvTracker.refreshTrack(frame,detection)
-                while(not self.trackerQ.empty()):
-                    print("track 111")
-                    frame = self.trackerQ.get()
-                    (success, boxes) = self.cvTracker.track(frame)
-                    self.draw(frame,boxes,detection)
-            elif(self.processingCNN.value):
-                while (not self.trackerQ.empty()):
-                    print("track 222")
-                    frame = self.trackerQ.get()
-                    (success, boxes) = self.cvTracker.track(frame)
-                    print("track 333")
-                    self.draw(frame, boxes, detection)
+            # elif(self.firstDetect.value):
+            while (not self.trackerQ.empty()):
+                frame = self.trackerQ.get()
+                (success, boxes) = self.cvTracker.track(frame)
+                self.draw(frame, boxes, detection)
 
     def draw(self, frame, boxes, detection):
         for box in boxes:
@@ -73,6 +70,7 @@ class Arbiter:
         key = cv2.waitKey(1) & 0xFF
 
 arbiter = Arbiter('ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.prototxt','ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.caffemodel','mosse')
+
 cap = cv2.VideoCapture(1)
 
 while(cap.isOpened()):
