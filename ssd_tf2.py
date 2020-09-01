beea315ec629379c2cd4846b2f7b6425f721a8cf
Milad.Niazy <milad.niazy@partsoftware.com>
2020-09-01 12:33:52 +0430

    limit mem

--- ssd_tf2.py
+++ ssd_tf2.py
@@ -32,8 +32,8 @@ from tensorflow.python.compiler.tensorrt import trt_convert as trt
 # tf.config.gpu.set_per_process_memory_fraction(0.75)
 # tf.config.gpu.set_per_process_memory_growth(True)
 #
-# gpus = tf.config.experimental.list_physical_devices('GPU')
-# tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
 # gpus = tf.config.experimental.list_physical_devices('GPU')
 # if gpus:
 #   try:
@@ -64,35 +64,58 @@ def load_image_into_numpy_array(path):
   (im_width, im_height) = image.size
   return np.array(image.getdata()).reshape(
       (im_height, im_width, 3)).astype(np.uint8)
-def inference(i,return_dict):
-  print ('number: ',str(i))
-  detect_fn  = tf.saved_model.load("tf_pretrainedModel/ssd_mobilenet_v2_coco_2018_03_29/saved_model")
-  print("model size: ", str(sys.getsizeof(detect_fn)))
-  print(list(detect_fn.signatures.keys()))
-  infer = detect_fn.signatures["serving_default"]
-  print(infer.structured_outputs)
-  print("infer size: ", sys.getsizeof(infer))
-  gc.collect()
-  # return_dict[0]=infer
-  im = cv2.imread("test_images/image1.jpg")
-  ##cv2_imshow(im)
-  # image_np = load_image_into_numpy_array("/content/drive/My Drive/Colab Notebooks/dog.jpg")
-  input_tensor = np.expand_dims(im, axis=0)
-  start_time = time.time()
-  detections = infer(tf.constant(input_tensor))
-  end_time = time.time()
-  t1 = end_time - start_time
-  print("inference time: ", str(end_time - start_time))
 
-  # print(detections)
-  bb = detections['detection_boxes']
-  c = detections['detection_classes'][0].numpy().astype(np.int32)
-  s = detections['detection_scores'][0].numpy()
-  print(c)
-  print(s)
+detect_fn  = tf.saved_model.load("tf_pretrainedModel/ssd_mobilenet_v2_coco_2018_03_29/saved_model")
+print("model size: ", str(sys.getsizeof(detect_fn)))
+print(list(detect_fn.signatures.keys()))
+infer = detect_fn.signatures["serving_default"]
+print(infer.structured_outputs)
+print("infer size: ", sys.getsizeof(infer))
+gc.collect()
+im = cv2.imread("test_images/image1.jpg")
+##cv2_imshow(im)
+input_tensor = np.expand_dims(im, axis=0)
+start_time = time.time()
+detections = infer(tf.constant(input_tensor))
+end_time = time.time()
+t1 = end_time - start_time
+print("inference time: ", str(end_time - start_time))
 
-manager = multiprocessing.Manager()
-return_dict = manager.dict()
-process_eval = multiprocessing.Process(target=inference, args=(0,return_dict))
-process_eval.start()
-process_eval.join()
+# print(detections)
+bb = detections['detection_boxes']
+c = detections['detection_classes'][0].numpy().astype(np.int32)
+s = detections['detection_scores'][0].numpy()
+print(c)
+print(s)
+# def inference(i,return_dict):
+#   print ('number: ',str(i))
+#   detect_fn  = tf.saved_model.load("tf_pretrainedModel/ssd_mobilenet_v2_coco_2018_03_29/saved_model")
+#   print("model size: ", str(sys.getsizeof(detect_fn)))
+#   print(list(detect_fn.signatures.keys()))
+#   infer = detect_fn.signatures["serving_default"]
+#   print(infer.structured_outputs)
+#   print("infer size: ", sys.getsizeof(infer))
+#   gc.collect()
+#   # return_dict[0]=infer
+#   im = cv2.imread("test_images/image1.jpg")
+#   ##cv2_imshow(im)
+#   # image_np = load_image_into_numpy_array("/content/drive/My Drive/Colab Notebooks/dog.jpg")
+#   input_tensor = np.expand_dims(im, axis=0)
+#   start_time = time.time()
+#   detections = infer(tf.constant(input_tensor))
+#   end_time = time.time()
+#   t1 = end_time - start_time
+#   print("inference time: ", str(end_time - start_time))
+#
+#   # print(detections)
+#   bb = detections['detection_boxes']
+#   c = detections['detection_classes'][0].numpy().astype(np.int32)
+#   s = detections['detection_scores'][0].numpy()
+#   print(c)
+#   print(s)
+#
+# manager = multiprocessing.Manager()
+# return_dict = manager.dict()
+# process_eval = multiprocessing.Process(target=inference, args=(0,return_dict))
+# process_eval.start()
+# process_eval.join()
