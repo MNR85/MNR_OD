63efa692e11a85b2d50b499ec827a98d33485390
Milad.Niazy <milad.niazy@partsoftware.com>
2020-09-04 18:02:54 +0430

    add tegra support for camera. video file still sucks

--- caffeOD.py
+++ caffeOD.py
@@ -3,12 +3,15 @@ import cv2
 from imutils.video import FPS
 
 arbiter = Arbiter('ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.prototxt','ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.caffemodel', True, 'mosse') #'mosse')
-
-cap = cv2.VideoCapture("test_images/soccer_01.mp4")#"test_images/soccer_01.mp4")#1)
+gst_str = ('v4l2src device=/dev/video{} ! '
+               'video/x-raw, width=(int){}, height=(int){} ! '
+               'videoconvert ! appsink').format(1, 1920, 1080)
+cap = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+# cap = cv2.VideoCapture("test_images/soccer_01.mp4")#"test_images/soccer_01.mp4")#1)
 fps = FPS().start()
 counter=0
 try:
-    while(cap.isOpened() and counter < 100):
+    while(cap.isOpened() and counter < 20):
         ret, frame = cap.read()
         if ret == True:
             fps.update()
