6b566e17fd4ee558c2198f576931c2e82a3f1fe3
Milad.Niazy <milad.niazy@partsoftware.com>
2020-09-04 11:20:42 +0430

    add some log, fix cnn gpu init bug

--- arbiter.py
+++ arbiter.py
@@ -3,9 +3,10 @@ from multiprocessing import Process, Value, Queue
 from imutils.video import FPS
 import cvTracker
 import MNR_Net
+import time
 
 class Arbiter:
-    def __init__(self, prototxt, model, trackType):
+    def __init__(self, prototxt, model, useGpu, trackType):
         self.runThread = Value('b', True)
         self.imageQ = Queue(maxsize=0)
         self.detectorInQ = Queue(maxsize=0)
@@ -15,10 +16,12 @@ class Arbiter:
         self.resultQ = Queue(maxsize=0)
         self.cvTracker =  cvTracker.cvTracker(trackType)
         self.detector = MNR_Net.Detector(prototxt, model)
-        self.detector.setRunMode(False)
-        self.detector.initNet()
+        self.useGpu = useGpu
+        self.initCNN = Value('b', False)
         self.processingCNN = Value('b', False)
-        # self.firstDetect = Value('b', False)
+        self.CnnCounter = Value('i', 0)
+        self.TrackCounter = Value('i', 0)
+        self.resultCounter = Value('i', 0)
         self.detectorP = Process(name='dnn', target=self.detectorThread)  # , args=[detector.runThread])
         self.detectorP.daemon = True  # background run
         self.trackerP = Process(name='track', target=self.trackerThread)  # , args=[detector.runThread])
@@ -28,7 +31,11 @@ class Arbiter:
         self.detectorP.start()
         self.trackerP.start()
         self.getResultP.start()
-        self.counter=0
+        print('waiting for init net...')
+        while not self.initCNN.value:
+            time.sleep(0.1)
+        print('Ready')
+
 
     def stop(self):
         self.runThread.value=False
@@ -37,7 +44,6 @@ class Arbiter:
         self.getResultP.join()
 
     def newImage(self, frame):
-        self.counter=self.counter+1
         if(not self.processingCNN.value): # and self.counter%60==0):
             # print("imageQ ", self.imageQ.qsize(), "detectQ ", self.detectorOutQ.qsize(), "TrackerQ ",self.trackerQ.qsize())
             while(not self.trackerQ.empty()): #empty Q
@@ -52,6 +58,9 @@ class Arbiter:
         self.trackerQF.put(True)
 
     def detectorThread(self):
+        self.detector.setRunMode(self.useGpu)
+        self.detector.initNet()
+        self.initCNN.value=True
         while (self.runThread.value):
             if (not self.detectorInQ.empty()):
                 self.processingCNN.value=True
@@ -59,6 +68,7 @@ class Arbiter:
                 detections = self.detector.serialDetector(frame)
                 self.detectorOutQ.put(detections['detection_out'])
                 self.processingCNN.value = False
+                self.CnnCounter.value=self.CnnCounter.value+1
 
     def trackerThread(self):
         detection=[0, 0, 0, 0]
@@ -73,23 +83,22 @@ class Arbiter:
                 (success, boxes) = self.cvTracker.track(frame)
                 if(firstTime):
                     self.resultQ.put(self.draw(frame, boxes, detection))
+                    self.TrackCounter.value=self.TrackCounter.value+1
 
     def getResultThread(self):
         fps = FPS().start()
-        # counter=0
         while (self.runThread.value):
             if(not self.resultQ.empty()):
                 fps.update()
                 im=self.resultQ.get()
+                self.resultCounter.value = self.resultCounter.value+1
                 # cv2.imwrite("test_images/"+str(counter)+".jpg", im)
                 # cv2.imshow("tracker",im)
                 # cv2.waitKey(1)
-                # counter=counter+1
         fps.stop()
         print("Output frame:")
         print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
         print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
 
 
     def draw(self, frame, boxes, detection):
@@ -99,13 +108,13 @@ class Arbiter:
         return frame
 
 
-arbiter = Arbiter('ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.prototxt','ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.caffemodel','mosse') #'mosse')
+arbiter = Arbiter('ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.prototxt','ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.caffemodel', True, 'mosse') #'mosse')
 
 cap = cv2.VideoCapture(1)
 fps = FPS().start()
 counter=0
 try:
-    while(cap.isOpened()):
+    while(cap.isOpened() and counter < 20):
         ret, frame = cap.read()
         if ret == True:
             fps.update()
@@ -125,4 +134,5 @@ cv2.destroyAllWindows()
 print("Input frame:")
 print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-print("Frame processed: ",str(arbiter.counter)," ,Frame Input: "+str(counter))
+print("Frame Input: "+str(counter))
+print("Result counter: ", str(arbiter.resultCounter.value),"CnnCounter counter: ", str(arbiter.CnnCounter.value),"TrackCounter counter: ", str(arbiter.TrackCounter.value))
