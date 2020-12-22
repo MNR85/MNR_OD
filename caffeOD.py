5b5542cc068a6123606a3788eeb52f4f6ad19933
mnr <milad.n.r.85@gmail.com>
2020-12-22 18:11:45 +0330

    add run.sh and eval

--- caffeOD.py
+++ caffeOD.py
@@ -14,40 +14,36 @@ parser.add_argument("-s", "--serial", required=False,
                     action='store_true', help="Serial or parallel detection")
 parser.add_argument("-e", '--eval', required=False,
                     action='store_true', help="Enable evaluation")
-parser.add_argument("-r", "--ratio", required=False, type=str,
-                    default='10', help="Detect track ratio for fixed ratio", metavar="FILE")
+parser.add_argument("-r", "--ratio", required=False, type=int,
+                    help="Detect track ratio for fixed ratio")
 parser.add_argument("-t", "--tracker", required=False, type=str,
                     default='mosse', help="path to video input file", metavar="FILE")
 parser.add_argument("-v", "--video", required=False, type=str,
                     default='test_images/ILSVRC/ILSVRC2017_train_00006000.mp4', help="path to video input file", metavar="FILE")
 parser.add_argument("-a", "--annotation", required=False, type=str,
-                    default='test_images/ILSVRC/ILSVRC2017_train_00006000_an/', help="path to annotation folder", metavar="FILE")
+                    default='test_images/ILSVRC/ILSVRC2017_train_00006000/', help="path to annotation folder", metavar="FILE")
 parser.add_argument("-p", "--prototxt", required=False, type=str,
                     default='ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.prototxt',
                     help="path to Caffe 'deploy' prototxt file", metavar="FILE")
 parser.add_argument("-m", "--model", required=False, type=str,
                     default='ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.caffemodel',
                     help="path to Caffe pre-trained model", metavar="FILE")
-
+args = vars(parser.parse_args())
 # parser.add_argument("-f", "--frame", required=False,
 #                     type=int, default=20, help="Frame count")
 # videoName="test_images/los_angeles.mp4"
-# useGPU=True
-fixedRatio=False
+fixedRatio=not (args['ratio'] is None)
 fixedTracker=True
-ratio=10
-eval=True
-serial=False
-gpu=True
+# ratio=10
+# eval=True
+# serial=False
+# gpu=True
 # trackType='csrt'
-# protoFile='ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.prototxt'
-# caffeModel='ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.caffemodel'
-args = vars(parser.parse_args())
 
 # -------------- Objs
 logger=MNR_logger("results")
 
-arbiter = Arbiter(args['prototxt'], args['model'], gpu, serial, args['tracker'],fixedTracker, logger, eval, args['annotation'], ratio, fixedRatio, )
+arbiter = Arbiter(args['prototxt'], args['model'], args['gpu'], args['serial'], args['tracker'],fixedTracker, logger, args['eval'], args['annotation'], args['ratio'], fixedRatio, )
 # gst_str = ('v4l2src device=/dev/video{} ! '
 #                'video/x-raw, width=(int){}, height=(int){} ! '
 #                'videoconvert ! appsink').format(1, 1920, 1080)
--- run.sh
+++ run.sh
@@ -1,44 +1,28 @@
-echo "mosse"
-python3 caffeOD.py -v test_images/retail.mp4 -t mosse
-python3 caffeOD.py -v test_images/los_angeles.mp4 -t mosse
-python3 caffeOD.py -v test_images/chair.mp4 -t mosse
-echo "mosse dwc"
-python3 caffeOD.py -v test_images/retail.mp4 -t mosse -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-python3 caffeOD.py -v test_images/los_angeles.mp4 -t mosse -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-python3 caffeOD.py -v test_images/chair.mp4 -t mosse -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-
-echo "kcf"
-python3 caffeOD.py -v test_images/retail.mp4 -t kcf
-python3 caffeOD.py -v test_images/los_angeles.mp4 -t kcf
-python3 caffeOD.py -v test_images/chair.mp4 -t kcf
-echo "kcf dwc"
-python3 caffeOD.py -v test_images/retail.mp4 -t kcf -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-python3 caffeOD.py -v test_images/los_angeles.mp4 -t kcf -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-python3 caffeOD.py -v test_images/chair.mp4 -t kcf -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-
-echo "tld"
-python3 caffeOD.py -v test_images/retail.mp4 -t tld
-python3 caffeOD.py -v test_images/los_angeles.mp4 -t tld
-python3 caffeOD.py -v test_images/chair.mp4 -t tld
-echo "tld dwc"
-python3 caffeOD.py -v test_images/retail.mp4 -t tld -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-python3 caffeOD.py -v test_images/los_angeles.mp4 -t tld -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-python3 caffeOD.py -v test_images/chair.mp4 -t tld -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-
-echo "medianflow"
-python3 caffeOD.py -v test_images/retail.mp4 -t medianflow
-python3 caffeOD.py -v test_images/los_angeles.mp4 -t medianflow
-python3 caffeOD.py -v test_images/chair.mp4 -t medianflow
-echo "medianflow dwc"
-python3 caffeOD.py -v test_images/retail.mp4 -t medianflow -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-python3 caffeOD.py -v test_images/los_angeles.mp4 -t medianflow -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-python3 caffeOD.py -v test_images/chair.mp4 -t medianflow -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-
-echo "csrt"
-python3 caffeOD.py -v test_images/retail.mp4 -t csrt
-python3 caffeOD.py -v test_images/los_angeles.mp4 -t csrt
-python3 caffeOD.py -v test_images/chair.mp4 -t csrt
-echo "csrt dwc"
-python3 caffeOD.py -v test_images/retail.mp4 -t csrt -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-python3 caffeOD.py -v test_images/los_angeles.mp4 -t csrt -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
-python3 caffeOD.py -v test_images/chair.mp4 -t csrt -p ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt
+#! /bin/bash
+echo "New run at $(date)">>results/runLog.log
+trackers=("mosse" "kcf" "tld" "medianflow" "csrt")
+prototxt=("ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.prototxt" "ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy_dwc.prototxt")
+evalData=("test_images/ILSVRC/ILSVRC2017_train_00006000" "test_images/ILSVRC/ILSVRC2017_train_00024000" "test_images/ILSVRC/ILSVRC2017_train_00066000")
+hw=("gpu" "cpu")
+methode=("serial" "pipeline")
+for p in "${prototxt[@]}"; do
+    pFile="-p $p"
+    for e in "${evalData[@]}"; do
+        vFile="-v ${e}.mp4 -a ${e}/"
+        for h in "${hw[@]}"; do
+            if [ $h = "gpu" ]; then
+                gMode="-g"
+            else
+                gMode=""
+            fi
+            for m in "${methode[@]}"; do
+                if [ $m = "serial" ]; then
+                    sMode="-s"
+                else
+                    sMode=""
+                fi
+                echo "python3 caffeOD.py $pFile $vFile $gMode $sMode">>results/runLog.log
+            done
+        done
+    done
+done
