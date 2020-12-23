5e50c951606b7d16295f2b5847790d8720d3e192
mnr <milad.n.r.85@gmail.com>
2020-12-23 09:49:48 +0330

    add eval to run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,8 @@ prototxt=("ssd_mobilenet_v1_coco_2017_11_17/MobileNetSSD_deploy.prototxt" "ssd_m
 evalData=("test_images/ILSVRC/ILSVRC2017_train_00006000" "test_images/ILSVRC/ILSVRC2017_train_00024000" "test_images/ILSVRC/ILSVRC2017_train_00066000")
 hw=("gpu" "cpu")
 methode=("serial" "pipeline")
-fixedRatio=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 20 25) # not support for now. should add
+#fixedRatio=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 20 25)
+fixedRatio=(10)
 for p in "${prototxt[@]}"; do
     pFile="-p $p"
     for e in "${evalData[@]}"; do
@@ -28,8 +29,7 @@ for p in "${prototxt[@]}"; do
                 fi
                 for r in "${fixedRatio[@]}"; do
                     ratio="-r $r"
-                    cmd="python3 caffeOD.py $pFile $vFile $gMode $sMode $ratio"
-                    a="python3 caffeOD.py"
+                    cmd="python3 caffeOD.py -d -e $pFile $vFile $gMode $sMode $ratio"
                     echo "$cmd"
                     echo "$cmd">>results/runLog.log
                     eval $cmd
