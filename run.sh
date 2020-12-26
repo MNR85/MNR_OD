ff409607562c4d010d0d7415aae4d148fa4d66d6
mnr <milad.n.r.85@gmail.com>
2020-12-26 18:31:13 +0330

    change run.sh

--- run.sh
+++ run.sh
@@ -14,39 +14,6 @@ hw=("gpu" "cpu")
 methode=("serial" "pipeline")
 fixedRatio=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 20 25)
 currentPoint=0
-echo "Dynamic portion $(date)">>results/runLog.log
-for p in "${prototxt[@]}"; do
-    pFile="-p $p "
-    for e in "${evalData[@]}"; do
-        vFile="-v ${e}.mp4 -a ${e}/ "
-        for h in "${hw[@]}"; do
-            if [ $h = "gpu" ]; then
-                gMode="-g "
-            else
-                gMode=""
-            fi
-            for m in "${methode[@]}"; do
-                if [ $m = "serial" ]; then
-                    sMode="-s "
-                else
-                    sMode=""
-                fi
-                if [[ $currentPoint -gt $checkPoint ]]; then
-                    cmd="python3 caffeOD.py $pFile$vFile$gMode$sMode"
-                    cmdDebug="$cmd-d -e"
-                    echo "$currentPoint - $cmd"
-                    echo "$currentPoint - $cmd">>results/runLog.log
-                    eval $cmd
-                    echo "$currentPoint - $cmdDebug"
-                    echo "$currentPoint - $cmdDebug">>results/runLog.log
-                    eval $cmdDebug
-                fi
-                currentPoint=$((currentPoint+1))
-                echo $currentPoint>results/checkPoint
-            done
-        done
-    done
-done
 echo "Static portion $(date)">>results/runLog.log
 for p in "${prototxt[@]}"; do
     pFile="-p $p"
@@ -68,12 +35,12 @@ for p in "${prototxt[@]}"; do
                     ratio="-r $r"
                     if [[ $currentPoint -gt $checkPoint ]]; then
                       cmd="python3 caffeOD.py $pFile $vFile $gMode $sMode $ratio"
-                      cmdDebug="$cmd -d -e"
+                      cmdEval="$cmd-e"
+                      cmdDebug="$cmd-d -e"
                       echo "$currentPoint - $cmd"
                       echo "$currentPoint - $cmd">>results/runLog.log
                       eval $cmd
-                      echo "$currentPoint - $cmdDebug"
-                      echo "$currentPoint - $cmdDebug">>results/runLog.log
+                      eval $cmdEval
                       eval $cmdDebug
                     fi
                     currentPoint=$((currentPoint+1))
